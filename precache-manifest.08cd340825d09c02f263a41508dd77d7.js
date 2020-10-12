1ede70f07b51826ca383698d3627d7e28c566dc3
nithin chandra <nithinchandra1996@gmail.com>
2020-10-12 11:53:48 -0400

    Updates

---
+++ precache-manifest.08cd340825d09c02f263a41508dd77d7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc0ff59cb1ad2a1fe290035e5b384cfc",
+    "url": "/CoronaTracker/index.html"
+  },
+  {
+    "revision": "f4a49afb69da5410b981",
+    "url": "/CoronaTracker/static/css/main.c03582d6.chunk.css"
+  },
+  {
+    "revision": "8bc2ae08c081b0ca019a",
+    "url": "/CoronaTracker/static/js/2.5c97dbb6.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/CoronaTracker/static/js/2.5c97dbb6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4a49afb69da5410b981",
+    "url": "/CoronaTracker/static/js/main.2a05be73.chunk.js"
+  },
+  {
+    "revision": "988fb36a40ced2d0eb6f",
+    "url": "/CoronaTracker/static/js/runtime-main.867b7744.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/CoronaTracker/static/media/image.d7265326.png"
+  }
+]);
